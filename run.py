73d0cf5cc127a8e5331b5c98738791d57244dfb1
unknown <keeelho@gmail.com>
2020-12-21 14:11:53 +0900

    chromedriver, selenium

--- run.py
+++ run.py
@@ -5,4 +5,12 @@ driver = webdriver.Chrome(path)
 
 driver.get("https://www.google.com")
 print(driver.title)
+
+search = driver.find_element_by_name("s")
+search.send_keys("test")
+search.send_keys(Keys.RETURN)
+
+main = driver.find_element_by_id("main")
+prinT(main.text)
+
 driver.quit()
